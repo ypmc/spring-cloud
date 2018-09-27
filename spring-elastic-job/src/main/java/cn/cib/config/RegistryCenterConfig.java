8c838f9c6968108cc3a7ca42b7a509663c17f7ed
ypmc <loukangkang@sina.cn>
2018-09-27 15:57:46 +0800

    'cluster'

--- spring-elastic-job/src/main/java/cn/cib/config/RegistryCenterConfig.java
+++ spring-elastic-job/src/main/java/cn/cib/config/RegistryCenterConfig.java
@@ -6,11 +6,13 @@ package cn.cib.config;
 
 import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperConfiguration;
 import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class RegistryCenterConfig {
 
     @Value("${reg-center.server-list}")
@@ -20,6 +22,7 @@ public class RegistryCenterConfig {
 
     @Bean(initMethod = "init")
     public ZookeeperRegistryCenter regCenter() {
+        log.info("serverLists is {}", serverList);
         return new ZookeeperRegistryCenter(new ZookeeperConfiguration(serverList, namespace));
     }
 
